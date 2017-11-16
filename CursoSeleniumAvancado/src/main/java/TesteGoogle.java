175d130adfdedfca1b99aabaf5ea6352e4664ca6
anderson <anderson.civil@hotmail.com>
2017-11-16 18:16:21 -0200

    first commit

---
+++ CursoSeleniumAvancado/src/main/java/TesteGoogle.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+
+	@Test
+	public void test() {
+		// System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://google.com");
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
+	}
+
+}
