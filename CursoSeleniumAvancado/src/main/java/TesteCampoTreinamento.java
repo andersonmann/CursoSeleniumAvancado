0ca7be6b3d6104859747cbae3f827b3235e4fd33
anderson <anderson.civil@hotmail.com>
2017-11-16 23:09:53 -0200

    elementos basicos

--- CursoSeleniumAvancado/src/main/java/TesteCampoTreinamento.java
+++ CursoSeleniumAvancado/src/main/java/TesteCampoTreinamento.java
@@ -1,7 +1,106 @@
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
-	
 
+	@Test
+	public void testeTextField() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Anderson Mann");
+		Assert.assertEquals("Anderson Mann", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		// Thread.sleep(5000);
+		driver.quit();
 	}
 
+	@Test
+	public void deveInteragirComTextArea() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste");
+		Assert.assertEquals("teste", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		Thread.sleep(3000);
+		driver.quit();
+	}
 
+	@Test
+	public void deveInteragirComRadioButton() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+	@Test
+	public void deveInteragirComCheckBox() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+	@Test
+	public void deveInteragirComCombo() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		// combo.selectByIndex(2);
+		// combo.selectByValue("superior");
+		combo.selectByVisibleText("2o grau completo");
+		Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+	@Test
+	public void deveVerificarValoresCombo(){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		List<WebElement> options = combo.getOptions();
+		Assert.assertEquals(8, options.size());
+
+		boolean encontrou = false;
+		for (WebElement option : options) {
+			if (option.getText().equals("Mestrado")) {
+				encontrou = true;
+				break;
+			}
+		}
+		Assert.assertTrue(encontrou);
+		driver.quit();
+	}
+
+	@Test
+	public void deveVerificarValoresComboMultiplo(){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		List<WebElement> allSelectOptins = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectOptins.size());
+		
+		combo.deselectByVisibleText("Corrida");
+		allSelectOptins =  combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectOptins.size());
+		driver.quit();
+	}
+
+}
